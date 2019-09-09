15a12212bf34d11764709298b24e0ebb62000f06
ARArulselvi <39916902+ARArulselvi@users.noreply.github.com>
2019-09-09 20:01:59 +0530

    Modified Admin UI

--- vericab_admin/src/ShowDrivers/ShowDrivers.react.js
+++ vericab_admin/src/ShowDrivers/ShowDrivers.react.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
+import { UncontrolledCollapse, Button, CardBody, Card } from 'reactstrap';
 import './ShowDrivers.css';
 import { getListByThunk, deleteById } from '../Reducer';
 
@@ -25,16 +25,52 @@ const ShowDrivers = (props) => {
     <div>
         {  
                         props.list.map( (eachDriver, index) => {
-                            return <div className="card cardStyle shadow p-3 mb-5">
+                            return <div className="card cardStyle shadow p-3 mb-5" key={index}>
                         {/* <div class="card-header"> </div> */}
-                        <div className="card-body" key="index">
+                        <div className="card-body" >
                                 <div className="row">
                                     <h6 className="DateInfoStyle col"> {eachDriver.id}  {eachDriver.name} </h6>
                                     {/* <h6 className="DateInfoStyle"> {eachDriver.name}</h6> */}
-                                    <a href="#display" data-toggle="collapse">View Details</a>
+                                   
+                                    <Button color="default" id="toggler1" style={{ marginBottom: '1rem' }}> ViewDetails </Button>
+                                        
+                                        
                                     <button onClick={()=>props.delete(eachDriver.id)}>Delete</button>
                                 </div>
+
+                                {/* <div id="display" className="collapse">
+                                    helllooo
+                                </div> */}
                         </div>
+                        <UncontrolledCollapse toggler="#toggler1" key={index}>
+                                <Card>
+                                    <CardBody>
+                                        <div className="row">
+                                            <div className="col">Name : {eachDriver.gender}</div>
+                                        </div>
+
+                                        <div className="row">
+                                            <div className="col">Rating : {eachDriver.rating}</div>
+                                        </div>
+
+                                        <div className="row">
+                                            <div className="col">Email : {eachDriver.email}</div>
+                                        </div>
+
+                                        <div className="row">
+                                            <div className="col">Phone Number : {eachDriver.phoneNumber}</div>
+                                        </div>
+
+                                        <div className="row">
+                                            <div className="col">DOB : {eachDriver.dob}</div>
+                                        </div>
+
+                                        <div className="row">
+                                            <div className="col">Driver License No : {eachDriver.licenseNumber}</div>
+                                        </div>
+                                    </CardBody>
+                                </Card>
+                         </UncontrolledCollapse>
                         </div>
                         })}
                         </div>
